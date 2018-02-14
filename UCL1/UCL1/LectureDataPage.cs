db2e827f4c65490a42f97b526b2a64092f2ba74b
garrywma <garry@appxib.com>
2018-02-14 17:29:42 +0000

    Add files via upload

---
+++ UCL1/UCL1/LectureDataPage.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace UCL1
+{
+	public partial class LectureDataPage : Xamarin.Forms.Pages.ListDataPage
+	{
+		public LectureDataPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
